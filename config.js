d83924556d5ba22955fbb80c85abf17c1da0bc28
chencan <53136543+chencanfggz@users.noreply.github.com>
2023-08-02 11:43:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+{
+    "title":"辰残",
+    "displayTitle":"chencan",
+    "subTitle":"童话是一种生活态度，仅此而已。",
+    "homeUrl":"http://chencan.github.io",
+    "avatarUrl":"http://meekdai.com/avatar.jpg",
+    "faviconUrl":"http://meekdai.com/favicon.ico",
+    "email":"2692399806@qq.com",
+    "startSite":"02/16/2015",
+    "filingNum":"",
+    "singlePage":["link","about"],
+    "commentLabelColor":"#006b75",
+    "yearColorList":["#bc4c00", "#0969da", "#1f883d", "#A333D0"],
+    "i18n":"CN",
+    "GMEEK_VERSION":"v1.2"
+}
